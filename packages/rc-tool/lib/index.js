755e5f393af22b2f585252c8a1ae5705c8ed1dc1
buquan <1085161116@qq.com>
2022-08-02 13:52:37 +0800

    feat: multi

--- packages/rc-tool/lib/index.js
+++ packages/rc-tool/lib/index.js
@@ -1,10 +1,19 @@
 'use strict';
 
-
 function sum(a, b) {
     return a + b;
 }
 
+function sub(a, b) {
+    return a - b;
+}
+
+function multi(a, b) {
+    return a * b;
+}
+
 module.exports = {
-    sum
+    sum,
+    sub,
+    multi
 };
