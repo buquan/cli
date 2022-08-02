041dd416c4004da4222eaf09c70bf0fbd292779b
buquan <1085161116@qq.com>
2022-08-02 14:18:53 +0800

    feat: test

--- packages/rc-tool/lib/index.js
+++ packages/rc-tool/lib/index.js
@@ -4,7 +4,16 @@ function sum(a, b) {
     return a + b;
 }
 
+function sub(a, b) {
+    return a - b;
+}
+
+function multi(a, b) {
+    return a * b;
+}
 
 module.exports = {
-    sum
+    sum,
+    sub,
+    multi
 };
